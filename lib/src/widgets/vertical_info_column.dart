cbd031222ed8f18018d03c622501c3b526eda124
morfilJA <morfiljuan@gmail.com>
2025-04-04 14:22:53 -0300

    fix mod

--- lib/src/widgets/vertical_info_column.dart
+++ lib/src/widgets/vertical_info_column.dart
@@ -1,4 +1,60 @@
 // import 'package:pdf/pdf.dart';
+// import 'package:pdf/widgets.dart' as pw;
+// import 'dart:math';
+
+// class VerticalInfoColumn extends pw.StatelessWidget {
+//   final String ticketNumber;
+//   final String randomNumbers;
+
+//   VerticalInfoColumn({
+//     required this.ticketNumber,
+//     required this.randomNumbers,
+//   });
+
+//   @override
+//   pw.Widget build(pw.Context context) {
+//     return pw.Column(
+//       mainAxisAlignment: pw.MainAxisAlignment.center,
+//       children: [
+//         pw.Transform.rotate(
+//           angle: -pi / 2, // -90 grados en radianes
+//           child: pw.Container(
+//             width: 180,
+//             // Necesario para posicionar correctamente el contenido rotado
+//             padding: const pw.EdgeInsets.only(bottom: 50),
+//             child: pw.Column(
+//               crossAxisAlignment: pw.CrossAxisAlignment.start,
+//               children: [
+//                 pw.Text('NOMBRE Y APELLIDO:',
+//                     style: const pw.TextStyle(fontSize: 8)),
+//                 pw.SizedBox(height: 5),
+//                 pw.Text('DNI:', style: const pw.TextStyle(fontSize: 8)),
+//                 pw.SizedBox(height: 5),
+//                 pw.Text('N° CARTÓN: $ticketNumber',
+//                     style: pw.TextStyle(
+//                         fontSize: 8, fontWeight: pw.FontWeight.bold)),
+//                 pw.SizedBox(height: 5),
+//                 pw.Text('NÚMEROS DEL CARTÓN: $randomNumbers',
+//                     style: pw.TextStyle(
+//                         fontSize: 8, fontWeight: pw.FontWeight.bold)),
+//                 pw.SizedBox(height: 5),
+//                 pw.Text('OBSERVACIONES:',
+//                     style: const pw.TextStyle(fontSize: 8)),
+//                 pw.SizedBox(height: 5),
+//                 pw.Text('SORTEA EL DÍA 15/04/25 POR QUINIELA',
+//                     style: const pw.TextStyle(fontSize: 8)),
+//                 pw.Text('NOCTURNA DE SAN JUAN',
+//                     style: const pw.TextStyle(fontSize: 8)),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'dart:math';
 
@@ -14,36 +70,62 @@ class VerticalInfoColumn extends pw.StatelessWidget {
   @override
   pw.Widget build(pw.Context context) {
     return pw.Container(
-      width: 30,
+      padding: const pw.EdgeInsets.only(
+          left: 5, right: 20), // Más espacio horizontal
       child: pw.Column(
+        mainAxisAlignment: pw.MainAxisAlignment.center,
         children: [
           pw.Transform.rotate(
             angle: -pi / 2, // -90 grados en radianes
             child: pw.Container(
-              width: 180,
+              width: 200, // Ancho aumentado
+              // Necesario para posicionar correctamente el contenido rotado
+              padding: const pw.EdgeInsets.only(bottom: 60),
               child: pw.Column(
                 crossAxisAlignment: pw.CrossAxisAlignment.start,
                 children: [
-                  pw.Text('NOMBRE Y APELLIDO:',
-                      style: const pw.TextStyle(fontSize: 8)),
+                  pw.Container(
+                    width: 200, // Ancho fijo para evitar saltos de línea
+                    child: pw.Text('NOMBRE Y APELLIDO:',
+                        style: const pw.TextStyle(fontSize: 8)),
+                  ),
                   pw.SizedBox(height: 5),
-                  pw.Text('DNI:', style: const pw.TextStyle(fontSize: 8)),
+                  pw.Container(
+                    width: 200,
+                    child:
+                        pw.Text('DNI:', style: const pw.TextStyle(fontSize: 8)),
+                  ),
                   pw.SizedBox(height: 5),
-                  pw.Text('N° CARTÓN: $ticketNumber',
-                      style: pw.TextStyle(
-                          fontSize: 8, fontWeight: pw.FontWeight.bold)),
+                  pw.Container(
+                    width: 200,
+                    child: pw.Text('N° CARTÓN: $ticketNumber',
+                        style: pw.TextStyle(
+                            fontSize: 8, fontWeight: pw.FontWeight.bold)),
+                  ),
                   pw.SizedBox(height: 5),
-                  pw.Text('NÚMEROS DEL CARTÓN: $randomNumbers',
-                      style: pw.TextStyle(
-                          fontSize: 8, fontWeight: pw.FontWeight.bold)),
+                  pw.Container(
+                    width: 200,
+                    child: pw.Text('NÚMEROS DEL CARTÓN: $randomNumbers',
+                        style: pw.TextStyle(
+                            fontSize: 8, fontWeight: pw.FontWeight.bold)),
+                  ),
                   pw.SizedBox(height: 5),
-                  pw.Text('OBSERVACIONES:',
-                      style: const pw.TextStyle(fontSize: 8)),
+                  pw.Container(
+                    width: 200,
+                    child: pw.Text('OBSERVACIONES:',
+                        style: const pw.TextStyle(fontSize: 8)),
+                  ),
                   pw.SizedBox(height: 5),
-                  pw.Text('SORTEA EL DÍA 15/04/25 POR QUINIELA',
-                      style: const pw.TextStyle(fontSize: 8)),
-                  pw.Text('NOCTURNA DE SAN JUAN',
-                      style: const pw.TextStyle(fontSize: 8)),
+                  pw.Container(
+                    width: 200,
+                    child: pw.Text('SORTEA EL DÍA 15/04/25 POR QUINIELA',
+                        style: const pw.TextStyle(fontSize: 8)),
+                  ),
+                  pw.Container(
+                    width: 200,
+                    child: pw.Text('NOCTURNA DE SAN JUAN',
+                        style: const pw.TextStyle(fontSize: 8)),
+                  ),
                 ],
               ),
             ),
