47cd6e87e75d4a6bf9d0beb010bbf1ff287f60c4
morfilJA <morfiljuan@gmail.com>
2025-04-04 14:37:45 -0300

    fix mod

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,6 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Generador de Cartones de Rifa',
       theme: ThemeData(
         primarySwatch: Colors.blue,
